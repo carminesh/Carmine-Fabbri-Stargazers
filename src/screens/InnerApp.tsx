81f184c547306ad0715fa1ea45f81589a2dc462a
carminesh <carmineshoot5@outlook.it>
2023-01-24 08:04:44 +0100

    change(InnerApp): new state managment added to innerApp

--- src/screens/InnerApp.tsx
+++ src/screens/InnerApp.tsx
@@ -1,12 +1,18 @@
 import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import React, { useState } from 'react';
 import ColorsPalette from '../constants/ColorsPalette';
 import SearchSection from '../components/SearchSection';
+import ListSection from '../components/ListSection';
+import { Stargazer } from '../models/Stargazer';
 
 const InnerApp: React.FC = () => {
+    const [fetchedStargazers, setFetchedStargazers] = useState<Stargazer[]>();
+    const [isLoading, setIsLoading] = useState(false);
+
     return (
         <View style={styles.viewContianer}>
-            <SearchSection />
+            <SearchSection setFetchedStargazers={setFetchedStargazers} setIsLoading={setIsLoading} />
+            <ListSection data={fetchedStargazers} isLoading={isLoading} />
         </View>
     );
 };
