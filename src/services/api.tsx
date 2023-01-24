af9b2176356f18747f597c34dd5554f6cd15bdb4
carminesh <carmineshoot5@outlook.it>
2023-01-24 23:21:48 +0100

    removed unsed files

--- src/services/api.tsx
+++
@@ -1,3 +0,0 @@
-const BASE_URL: string = 'https://api.github.com';
-
-export const GET_LIST_STARGAZERS = (repoOwner: string, repoName: string) => BASE_URL + '/repos/' + repoOwner + '/' + repoName + '/stargazers?per_page=30';
