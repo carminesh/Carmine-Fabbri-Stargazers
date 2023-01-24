0b039a42da87d7cf596c9ab4b70e38b3cd9abbee
carminesh <carmineshoot5@outlook.it>
2023-01-24 20:28:41 +0100

    change(InnerApp): added new render memoid main section

--- src/screens/InnerApp.tsx
+++ src/screens/InnerApp.tsx
@@ -1,18 +1,26 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React, { useState } from 'react';
+import { StyleSheet, View } from 'react-native';
+import React, { useMemo, useState } from 'react';
 import ColorsPalette from '../constants/ColorsPalette';
 import SearchSection from '../components/SearchSection';
 import ListSection from '../components/ListSection';
 import { Stargazer } from '../models/Stargazer';
+import ExplanotarySection from '../components/ExplanotarySection';
 
 const InnerApp: React.FC = () => {
     const [fetchedStargazers, setFetchedStargazers] = useState<Stargazer[]>();
-    const [isLoading, setIsLoading] = useState(false);
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [showEplainer, setShowEplainer] = useState<boolean>(true);
+
+    /* This method allows the explanotary section to be rendered only the first time the user opens the app.
+        When there are no result, the ListSection will show a different image */
+    const renderMainSection = useMemo(() => {
+        return showEplainer ? <ExplanotarySection /> : <ListSection data={fetchedStargazers} isLoading={isLoading} />;
+    }, [fetchedStargazers, isLoading, showEplainer]);
 
     return (
         <View style={styles.viewContianer}>
-            <SearchSection setFetchedStargazers={setFetchedStargazers} setIsLoading={setIsLoading} />
-            <ListSection data={fetchedStargazers} isLoading={isLoading} />
+            <SearchSection setFetchedStargazers={setFetchedStargazers} setIsLoading={setIsLoading} setShowEplainer={setShowEplainer} />
+            {renderMainSection}
         </View>
     );
 };
