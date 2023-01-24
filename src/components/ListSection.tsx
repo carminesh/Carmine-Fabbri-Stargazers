94c947b1858bf0978b5fe4c4494ad9c51a37920f
carminesh <carmineshoot5@outlook.it>
2023-01-24 18:26:20 +0100

    change(ListSection): added conditional rendering

--- src/components/ListSection.tsx
+++ src/components/ListSection.tsx
@@ -1,9 +1,11 @@
-import { ActivityIndicator, ScaledSize, StyleSheet, useWindowDimensions, View } from 'react-native';
-import React, { useCallback } from 'react';
+import { ActivityIndicator, ScaledSize, StyleSheet, Text, useWindowDimensions, View } from 'react-native';
+import React, { useCallback, useMemo } from 'react';
 import ColorsPalette from '../constants/ColorsPalette';
+import { SvgXml } from 'react-native-svg';
 import { FlashList } from '@shopify/flash-list';
 import { Stargazer } from '../models/Stargazer';
 import UserCell from '../atoms/UserCell';
+import { noResultIcon } from '../assets/svg/noResultIcon';
 
 interface ListSectionProps {
     data: Stargazer[] | undefined;
@@ -14,6 +16,20 @@ const ListSection: React.FC<ListSectionProps> = ({ data, isLoading }) => {
     const dimensions = useWindowDimensions();
     const styles = style(dimensions);
 
+    const renderListSection = useMemo(() => {
+        if (data && data.length > 0) {
+            return <FlashList data={data} keyExtractor={(item, index) => index.toString()} renderItem={({ item }) => renderItem(item)} estimatedItemSize={300} numColumns={1} />;
+        } else {
+            return (
+                <View style={styles.svgContainer}>
+                    <SvgXml width={'45%'} height={'45%'} xml={noResultIcon()} />
+                    <Text style={styles.textHeader}>No results found</Text>
+                    <Text style={styles.textSubheader}>No one still liked this repository</Text>
+                </View>
+            );
+        }
+    }, [data]);
+
     const renderItem = useCallback(
         (item: Stargazer) => {
             return <UserCell login={item.login} id={item.id} avatar_url={item.avatar_url} />;
@@ -22,13 +38,7 @@ const ListSection: React.FC<ListSectionProps> = ({ data, isLoading }) => {
     );
 
     return (
-        <View style={[styles.containerView, isLoading && { justifyContent: 'center' }]}>
-            {isLoading ? (
-                <ActivityIndicator size="small" color={ColorsPalette.BLUE_ACCENT} />
-            ) : (
-                <FlashList data={data} keyExtractor={(item, index) => index.toString()} renderItem={({ item }) => renderItem(item)} estimatedItemSize={300} numColumns={1} />
-            )}
-        </View>
+        <View style={[styles.containerView, isLoading && { justifyContent: 'center' }]}>{isLoading ? <ActivityIndicator size="small" color={ColorsPalette.BLUE_ACCENT} /> : renderListSection}</View>
     );
 };
 
@@ -42,5 +52,21 @@ const style = (dimensions: ScaledSize) =>
             height: '100%',
             width: '100%',
             backgroundColor: ColorsPalette.WHITE90,
+        },
+        svgContainer: {
+            width: '100%',
+            height: '100%',
+            alignItems: 'center',
+            justifyContent: 'center',
+            paddingBottom: 24,
+        },
+        textHeader: {
+            fontWeight: 'bold',
+            fontSize: 20,
+            color: ColorsPalette.BLACK_ACCENT,
+        },
+        textSubheader: {
+            paddingTop: 6,
+            fontWeight: '400',
         },
     });
