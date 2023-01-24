3b61fad2b44d98784ba3e27e13e4ffa06818b16a
carminesh <carmineshoot5@outlook.it>
2023-01-24 19:12:31 +0100

    new(assets): added new images

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,5 @@
+import { ImageSourcePropType } from "react-native/types";
+
+export const SEARCH_ICON: ImageSourcePropType = require('../icons/searchIcon.png');
+export const USER_ICON: ImageSourcePropType = require('../icons/userIcon.png');
+export const REPO_ICON: ImageSourcePropType = require('../icons/repoIcon.png');
