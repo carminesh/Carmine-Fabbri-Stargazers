750610ea6cceb9b8051455a62c9e25af641f80e8
carminesh <carmineshoot5@outlook.it>
2023-01-25 09:03:04 +0100

    change(ListSection): refactoring

--- src/components/ListSection.tsx
+++ src/components/ListSection.tsx
@@ -8,35 +8,37 @@ import UserCell from '../atoms/UserCell';
 import { noResultIcon } from '../assets/svg/noResultIcon';
 import ExplanotarySection from './ExplanotarySection';
 import { noStargazersIcon } from '../assets/svg/noStargazersIcon';
+import { useSelector } from 'react-redux';
+import { getStargazers } from '../store/slices/StargazersSlice';
 
 interface ListSectionProps {
-    data: Stargazer[] | undefined;
     isLoading: boolean;
 }
 
-const ListSection: React.FC<ListSectionProps> = ({ data, isLoading }) => {
+const ListSection: React.FC<ListSectionProps> = ({ isLoading }) => {
     const dimensions = useWindowDimensions();
     const styles = style(dimensions);
+    const fetchedStargazers: Stargazer[] | undefined = useSelector(getStargazers);
 
     const renderListSection = useMemo(() => {
         // Check if data is undefined
-        if (data === undefined) {
+        if (fetchedStargazers === undefined) {
             // If data is undefined, there was an error with the requested data so return the `ExplanotarySection` with error message
             return <ExplanotarySection svgImage={noResultIcon()} textHeader={'No results found'} textSubheader={'Enter the new data correctly and try again'} />;
-        } else if (data && data.length > 0) {
+        } else if (fetchedStargazers && fetchedStargazers.length > 0) {
             // If data is truthy and has a length greater than 0, return the `FlashList
-            return <FlashList data={data} keyExtractor={(item, index) => index.toString()} renderItem={({ item }) => renderItem(item)} estimatedItemSize={300} numColumns={1} />;
+            return <FlashList data={fetchedStargazers} keyExtractor={(item, index) => index.toString()} renderItem={({ item }) => renderItem(item)} estimatedItemSize={300} numColumns={1} />;
         } else {
             // If data is truthy but has a length of 0 this mean that no one starred the submitted repo
             return <ExplanotarySection svgImage={noStargazersIcon()} textHeader={'This repo is alone'} textSubheader={'No one still liked this repository'} />;
         }
-    }, [data]);
+    }, [fetchedStargazers]);
 
     const renderItem = useCallback(
         (item: Stargazer) => {
             return <UserCell login={item.login} id={item.id} avatar_url={item.avatar_url} />;
         },
-        [data]
+        [fetchedStargazers]
     );
 
     return (
