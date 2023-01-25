2a794553f1831f9990b9ff664a7723e2c8c294d9
carminesh <carmineshoot5@outlook.it>
2023-01-25 08:59:42 +0100

    change(App.tsx): added store provider

--- App.tsx
+++ App.tsx
@@ -2,16 +2,18 @@ import React from 'react';
 import { SafeAreaView, StyleSheet } from 'react-native';
 import ColorsPalette from './src/constants/ColorsPalette';
 import InnerApp from './src/screens/InnerApp';
+import { Provider } from 'react-redux';
+import store from './src/store/store';
 
 function App(): JSX.Element {
     return (
         /* In that way, we can obtain different colors on Top and Bottom in SafeAreaView component */
-        <>
+        <Provider store={store}>
             <SafeAreaView style={styles.container} />
             <SafeAreaView style={styles.container}>
                 <InnerApp />
             </SafeAreaView>
-        </>
+        </Provider>
     );
 }
 
