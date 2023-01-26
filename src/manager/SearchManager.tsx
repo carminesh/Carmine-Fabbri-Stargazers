1463fb4b1cc244f843ee687593e0a3fb0949d81f
carminesh <carmineshoot5@outlook.it>
2023-01-26 09:12:25 +0100

    change(SearchManager): changed value per page

--- src/manager/SearchManager.tsx
+++ src/manager/SearchManager.tsx
@@ -1,16 +1,17 @@
-import { authenticatedOctokit } from "../services/octokit";
-
+import { authenticatedOctokit } from '../services/octokit';
 
 /* As you requested I followed API specs adherence from the official docs by using octokit. */
 const getStargazersList = async (repoOwner: string, repoName: string) => {
     try {
-        let response = await authenticatedOctokit.request(`GET /repos/${repoOwner}/${repoName}/stargazers?per_page=30`, {
+        let response = await authenticatedOctokit.request(`GET /repos/${repoOwner}/${repoName}/stargazers?per_page=100`, {
             owner: repoOwner,
             repo: repoName,
         });
         return response.data;
     } catch (error: any) {
-        console.error('Error: ', error.response.data.message);
+        /*  The correct way to manage the error is to use the console.error or threw it, but for development purposes, I just left the console log. 
+        console.error('Error: ', error.response.data.message); */
+        console.log('Error: ', error.response.data.message);
     }
 };
 
